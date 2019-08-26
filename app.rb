ca0c89c734df82092a5b6f5e2d36176bee686d99
Vadim Dimnich <v.dimnich@gmail.com>
2019-08-27 00:17:48 +0300

    display error text

--- app.rb
+++ app.rb
@@ -35,9 +35,13 @@ end
 post '/visit' do
 	
 	c = Client.new params[:client]
-	c.save
 
-	erb "<h2>Вы записались</h2>"
+	if c.save
+		erb "<h2>Вы записались</h2>"
+	else
+		@error = c.errors.full_messages.first
+		erb :visit
+	end
 end 
 
 get '/contacts' do
